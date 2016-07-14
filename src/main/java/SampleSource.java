a46097c5299ff7affb57815640eaefc1250c6a22
Ulhas Pai <ulhas87@gmail.com>
2016-07-13 21:26:07 -0400

    Initial commit

---
+++ src/main/java/SampleSource.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by ulhas on 7/13/2016.
+ */
+public class SampleSource {
+
+    public void method1() {
+        //do nothing
+    }
+
+    public void method2() {
+        //do nothing
+    }
+}
---
+++ src/test/java/SampleSourceTest.java
@@ -0,0 +1,19 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by ulhas on 7/13/2016.
+ */
+public class SampleSourceTest extends TestCase {
+
+    public void testMethod1Case1() throws Exception {
+        SampleSource source = new SampleSource();
+        source.method1();
+        assertTrue(true);
+    }
+
+    public void testMethod1Case2() throws Exception {
+        SampleSource source = new SampleSource();
+        source.method1();
+        assertTrue(true);
+    }
+}
